e017a09dc1227a3e33b72c24d6ccbcf2f42a12c6
M. Gabriel <gabmar@post.cz>
2026-02-18 21:28:59 +0100

    Add files via upload

---
+++ martin_gabriel_research_questions.sql
@@ -0,0 +1,205 @@
+--1) Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
+
+-- 1. varianta: pokles mezd v daných letech
+SELECT DISTINCT
+	tmgpspf.year,
+	tmgpspf.industry_branch,
+	tmgpspf.nw_perc_growth,
+	tmgpspf.fte_perc_growth
+FROM
+	t_martin_gabriel_project_sql_primary_final tmgpspf
+WHERE 1=2
+	OR tmgpspf.nw_perc_growth <= 0
+	OR tmgpspf.fte_perc_growth <= 0
+ORDER BY
+	tmgpspf.industry_branch,
+	tmgpspf.year;
+
+-- 2. varianta: pokles mezd za celé měřené období
+WITH measured_wages AS (
+	SELECT DISTINCT
+		tmgpspf.industry_branch,
+		FIRST_VALUE(tmgpspf.year) OVER (
+			PARTITION BY tmgpspf.industry_branch
+			ORDER BY tmgpspf.year ASC) AS first_measured_year,
+		FIRST_VALUE(tmgpspf.year) OVER (
+			PARTITION BY tmgpspf.industry_branch
+			ORDER BY tmgpspf.year DESC) AS last_measured_year,
+		FIRST_VALUE(tmgpspf.nominal_wage) OVER (
+			PARTITION BY tmgpspf.industry_branch
+			ORDER BY tmgpspf.year ASC) AS first_measured_nw,
+		FIRST_VALUE(tmgpspf.nominal_wage) OVER (
+			PARTITION BY tmgpspf.industry_branch
+			ORDER BY tmgpspf.year DESC) AS last_measured_nw,
+		FIRST_VALUE(tmgpspf.fte_wage) OVER (
+			PARTITION BY tmgpspf.industry_branch
+			ORDER BY tmgpspf.year ASC) AS first_measured_fte,
+		FIRST_VALUE(tmgpspf.fte_wage) OVER (
+			PARTITION BY tmgpspf.industry_branch
+			ORDER BY tmgpspf.year DESC) AS last_measured_fte
+	FROM
+		t_martin_gabriel_project_sql_primary_final tmgpspf)
+SELECT
+	mw.industry_branch,
+	mw.first_measured_year,
+	mw.last_measured_year,
+	mw.first_measured_nw AS first_measured_nominal_wage,
+	mw.last_measured_nw AS last_measured_nominal_wage,
+	mw.first_measured_fte,
+	mw.last_measured_fte
+FROM
+	measured_wages mw
+WHERE 1=2
+	OR mw.first_measured_nw > mw.last_measured_nw
+	OR mw.first_measured_fte > mw.last_measured_fte;
+
+
+
+--2) Kolik je možné si koupit litrů mléka a kilogramů chleba
+--   za první a poslední srovnatelné období v dostupných datech cen a mezd?
+SELECT
+	tmgpspf.year,
+	tmgpspf.industry_branch,
+	tmgpspf.category_name,
+	tmgpspf.nominal_wage,
+	tmgpspf.avg_price,
+	ROUND(tmgpspf.nominal_wage / avg_price, 0) AS available_amount, --množství dostupných potravin
+	tmgpspf.price_unit
+FROM
+	t_martin_gabriel_project_sql_primary_final tmgpspf
+WHERE 1=1
+	AND tmgpspf.year IN (2006, 2018)
+	AND tmgpspf.category_name IN ('Chléb konzumní kmínový', 'Mléko polotučné pasterované')
+ORDER BY
+	tmgpspf.category_name,
+	tmgpspf.year,
+	tmgpspf.industry_branch;
+
+
+
+-- 3) Která kategorie potravin zdražuje nejpomaleji
+--    (je u ní nejnižší percentuální meziroční nárůst)?
+
+-- 1. varianta: nejpomalejší růst cen v daných letech
+SELECT
+	year,
+	category_name,
+	price_annual_perc_growth
+FROM
+	(SELECT DISTINCT
+		tmgpspf.year,
+		tmgpspf.category_name,
+		tmgpspf.price_annual_perc_growth,
+		RANK() OVER (
+			PARTITION BY tmgpspf.year
+			ORDER BY tmgpspf.price_annual_perc_growth) AS ranking
+	FROM
+		t_martin_gabriel_project_sql_primary_final tmgpspf
+	WHERE 1=1
+		AND tmgpspf.price_annual_perc_growth IS NOT NULL
+		-- Volitelný filtr: pouze kateogorie s neklesajícími cenami
+		--AND tmgpspf.price_annual_perc_growth > 0
+		)
+WHERE
+	ranking = 1
+ORDER BY 
+	year;
+
+-- 2.varianta: nejpomalejší růst cen za celé měřené období
+WITH measured_prices AS (
+	SELECT DISTINCT
+		tmgpspf.category_name,
+		FIRST_VALUE(tmgpspf.year) OVER (
+			PARTITION BY tmgpspf.category_name
+			ORDER BY tmgpspf.year ASC) AS first_measured_year,
+		FIRST_VALUE(tmgpspf.year) OVER (
+			PARTITION BY tmgpspf.category_name
+			ORDER BY tmgpspf.year DESC) AS last_measured_year,
+		FIRST_VALUE(tmgpspf.avg_price) OVER (
+			PARTITION BY tmgpspf.category_name
+			ORDER BY tmgpspf.year ASC) AS first_measured_price,
+		FIRST_VALUE(tmgpspf.avg_price) OVER (
+			PARTITION BY tmgpspf.category_name
+			ORDER BY tmgpspf.year DESC) AS last_measured_price
+	FROM
+		t_martin_gabriel_project_sql_primary_final tmgpspf)
+SELECT
+	mp.category_name,
+	ROUND((mp.last_measured_price / mp.first_measured_price) * 100 - 100, 2) AS total_perc_price_change,
+	mp.first_measured_year,
+	mp.last_measured_year,
+	mp.first_measured_price,
+	mp.last_measured_price
+FROM
+	measured_prices mp
+WHERE 1=1
+	-- Volitelný filtr: pouze kateogorie s neklesajícími cenami
+	--AND (mp.last_measured_price / mp.first_measured_price) - 1 > 0
+ORDER BY
+	total_perc_price_change
+LIMIT 1;
+
+
+
+--4) Existuje rok, ve kterém je rozdíl mezi růstem průměrných cen potravin a mezd vyšší než 10 procent?
+WITH stats_growth AS (
+	SELECT
+		avg_stats.year,
+		--avg_stats.avg_nw,
+		--avg_stats.avg_prices,
+		ROUND(avg_stats.avg_nw / LAG(avg_stats.avg_nw) OVER (
+			ORDER BY avg_stats.year) * 100 - 100, 0) AS avg_nom_wage_perc_growth,
+		ROUND(avg_stats.avg_prices / LAG(avg_stats.avg_prices) OVER (
+			ORDER BY avg_stats.year) * 100 - 100, 2) AS avg_prices_perc_growth
+	FROM (
+		SELECT
+			tmgpspf.year,
+			AVG(tmgpspf.nominal_wage) AS avg_nw,
+			AVG(tmgpspf.avg_price) AS avg_prices
+		FROM
+			t_martin_gabriel_project_sql_primary_final tmgpspf
+		GROUP BY
+			tmgpspf.year) AS avg_stats)
+SELECT 
+	*,
+	sg.avg_nom_wage_perc_growth - sg.avg_prices_perc_growth AS diff
+FROM
+	stats_growth sg
+WHERE
+	ABS(sg.avg_nom_wage_perc_growth - sg.avg_prices_perc_growth) > 10
+ORDER BY
+	sg.year;
+
+
+
+--5) Má výška HDP vliv na změny ve mzdách a cenách potravin?
+--   Neboli, pokud HDP vzroste výrazněji v jednom roce,
+--   projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
+SELECT
+	wages_and_prices.year,
+	wages_and_prices.avg_nw_perc_growth AS avg_nom_wage_perc_growth,
+	wages_and_prices.avg_prices_perc_growth,
+	gdp_growth.nom_gdp_growth
+FROM
+	(SELECT
+		tmgpspf.year,
+		ROUND(AVG(tmgpspf.nominal_wage::NUMERIC)/ LAG(AVG(tmgpspf.nominal_wage::NUMERIC)) OVER (
+			ORDER BY tmgpspf.year ) * 100 - 100, 2) AS avg_nw_perc_growth,
+		ROUND(AVG(tmgpspf.avg_price::NUMERIC)/ LAG(AVG(tmgpspf.avg_price::NUMERIC)) OVER (
+			ORDER BY tmgpspf.year ) * 100 - 100, 2) AS avg_prices_perc_growth
+	FROM
+		t_martin_gabriel_project_sql_primary_final tmgpspf
+	GROUP BY
+		tmgpspf.year) AS wages_and_prices
+INNER JOIN (
+	SELECT
+		tmgpssf.year,
+		ROUND(tmgpssf.gdp::numeric / LAG(tmgpssf.gdp::numeric) OVER (
+			ORDER BY tmgpssf.year) * 100 - 100, 2) AS nom_gdp_growth
+	FROM
+		t_martin_gabriel_project_SQL_secondary_final tmgpssf
+	WHERE
+		tmgpssf.country = 'Czech Republic') AS gdp_growth
+	ON wages_and_prices.year = gdp_growth.year
+ORDER BY
+	wages_and_prices.year;
---
+++ martin_gabriel_table_generation_script.sql
@@ -0,0 +1,127 @@
+-- ==========================================================
+-- table_generation_script – čtvrtý projekt do Engeto Online Akademie
+-- Autor: Martin Gabriel
+-- ==========================================================
+-- Tento skript vytváří dva pohledy a dvě výsledné tabulky.
+-- Pohledy musí být vytvořeny před vytvořením primární tabulky.
+-- Skript je navržen ke spuštění jako celek v uvedeném pořadí.
+
+-- Příkazy DROP IF EXISTS jsou uvedeny zakomentované nad
+-- jednotlivými objekty pro případ opakovaného spuštění.
+
+-- Po spuštění skriptu budou v databázi vytvořeny tyto objekty:
+--   VIEW  v_martin_gabriel_czechia_price_joined
+--   VIEW  v_martin_gabriel_czechia_payroll_joined
+--   TABLE t_martin_gabriel_project_SQL_primary_final
+--   TABLE t_martin_gabriel_project_SQL_secondary_final
+
+--Z těchto dat je možné spouštět skripty připravené k zodpovězení výzkumných otázek
+-- ==========================================================
+
+DROP VIEW IF EXISTS  v_martin_gabriel_czechia_price_joined;
+CREATE OR REPLACE VIEW v_martin_gabriel_czechia_price_joined AS (
+	SELECT
+		measured_year,
+		category_name,
+		avg_price,
+		ROUND(avg_price::numeric / LAG(avg_price::numeric) OVER (
+			PARTITION BY category_name
+			ORDER BY measured_year) * 100 - 100, 2) AS price_annual_perc_growth,
+		--ROUND(avg_price::numeric / FIRST_VALUE(avg_price::numeric) OVER (
+			--PARTITION BY category_name
+			--ORDER BY measured_year) * 100 - 100, 2) AS price_compound_perc_growth,
+		price_value,
+		price_unit
+	FROM
+		(SELECT
+			ROUND(AVG(cp.value)::numeric, 2) AS avg_price,
+			cpc.name AS category_name,
+			date_part('year', cp.date_from)::int AS measured_year,
+        	cpc.price_value,
+			cpc.price_unit
+        FROM
+           	czechia_price cp
+        LEFT JOIN czechia_price_category cpc
+        	ON cp.category_code = cpc.code
+        GROUP BY
+        	category_name,
+        	measured_year,
+        	price_value,
+        	price_unit
+        )
+	);
+
+
+DROP VIEW IF EXISTS v_martin_gabriel_czechia_payroll_joined;
+CREATE OR REPLACE VIEW v_martin_gabriel_czechia_payroll_joined AS (
+	SELECT
+		industry_branch,
+		nominal_wage,
+		ROUND(nominal_wage::numeric / LAG(nominal_wage::numeric) OVER (
+			PARTITION BY industry_branch
+			ORDER BY payroll_year) * 100 - 100, 2) AS NW_perc_growth,
+		full_time_equivalent,
+		ROUND(full_time_equivalent::numeric / LAG(full_time_equivalent::numeric) OVER (
+			PARTITION BY industry_branch
+			ORDER BY payroll_year) * 100 - 100, 2) AS FTE_perc_growth,
+		payroll_year
+	FROM
+		(SELECT
+			MAX(CASE WHEN cpc.code = 100 THEN value END) AS nominal_wage,
+    		MAX(CASE WHEN cpc.code = 200 THEN value END) AS full_time_equivalent,
+			cpib.name AS industry_branch,
+			cp.payroll_year
+		FROM
+			czechia_payroll cp
+			LEFT JOIN czechia_payroll_unit cpu 
+				ON cpu.code = cp.unit_code
+			LEFT JOIN czechia_payroll_calculation cpc 
+				ON cpc.code = cp.calculation_code
+			LEFT JOIN czechia_payroll_industry_branch cpib
+				ON cpib.code = cp.industry_branch_code 
+		WHERE 
+			cp.value_type_code = 5958
+			AND cp.value IS NOT NULL
+			AND cpib.name IS NOT NULL
+		GROUP BY
+			cpib.name,
+			cp.payroll_year
+		ORDER BY
+			industry_branch,
+			payroll_year)
+	);
+
+
+DROP TABLE IF EXISTS t_martin_gabriel_project_SQL_primary_final;
+CREATE TABLE t_martin_gabriel_project_SQL_primary_final AS (
+	SELECT
+		measured_year AS year,
+		industry_branch,
+		nominal_wage,
+		nw_perc_growth,
+		full_time_equivalent AS fte_wage,
+		fte_perc_growth,
+		category_name,
+		avg_price,
+		price_annual_perc_growth,
+		price_value,
+		price_unit
+	FROM
+		v_martin_gabriel_czechia_payroll_joined vmgcpj
+	INNER JOIN v_martin_gabriel_czechia_price_joined vmgcpj2 
+		ON vmgcpj.payroll_year = vmgcpj2.measured_year
+	);
+
+
+DROP TABLE IF EXISTS t_martin_gabriel_project_SQL_secondary_final;
+CREATE TABLE t_martin_gabriel_project_SQL_secondary_final AS (
+	SELECT
+		e.year,
+		e.country,
+		e.gdp,
+		e.population
+	FROM
+		economies e
+	WHERE
+		e.gdp IS NOT NULL
+	);
